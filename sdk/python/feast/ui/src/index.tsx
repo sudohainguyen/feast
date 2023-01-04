2ce44a0d58eb614363503726a0e1c3f0e2ba8497
Hai Nguyen <hai.nguyen12@mservice.com.vn>
2023-01-04 23:50:48 +0700

    temp fix
    Signed-off-by: Hai Nguyen <hai.nguyen12@mservice.com.vn>

--- sdk/python/feast/ui/src/index.tsx
+++ sdk/python/feast/ui/src/index.tsx
@@ -6,7 +6,17 @@ import "@feast-dev/feast-ui/dist/feast-ui.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <FeastUI />
+    <FeastUI
+    feastUIConfigs={{
+        projectListPromise: fetch("/plato/feature_platform/projects-list.json", {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        }).then((res) => {
+            return res.json();
+        })
+    }}
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
