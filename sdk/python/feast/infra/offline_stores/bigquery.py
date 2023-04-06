0c89f8aa881d5cfbf6a37937523e59f622243516
Hai Nguyen <quanghai.ng1512@gmail.com>
2023-04-06 21:21:16 +0700

    stuff: - feat: Add standalone sqlregistry config - fix: create result table within template query - chore: replace print with logging - feat: add option to overwrite persisted result - fix: adhoc increase timeout persisting - feat: improve infer speed and reduce cost
    Signed-off-by: Hai Nguyen <quanghai.ng1512@gmail.com>

--- sdk/python/feast/infra/offline_stores/bigquery.py
+++ sdk/python/feast/infra/offline_stores/bigquery.py
@@ -1,4 +1,5 @@
 import contextlib
+import logging
 import tempfile
 import uuid
 from datetime import date, datetime, timedelta
@@ -66,6 +67,9 @@ except ImportError as e:
     from feast.errors import FeastExtrasDependencyImportError
 
     raise FeastExtrasDependencyImportError("gcp", str(e))
+
+
+logger = logging.getLogger(__name__)
 
 
 def get_http_client_info():
@@ -457,6 +461,7 @@ class BigQueryRetrievalJob(RetrievalJob):
         self,
         job_config: Optional[bigquery.QueryJobConfig] = None,
         timeout: int = 1800,
+        allow_overwrite: bool = False,
         retry_cadence: int = 10,
     ) -> str:
         """
@@ -486,7 +491,7 @@ class BigQueryRetrievalJob(RetrievalJob):
                 self.to_df(), job_config.destination
             )
             job.result()
-            print(f"Done writing to '{job_config.destination}'.")
+            logger.info(f"Done writing to '{job_config.destination}'.")
             return str(job_config.destination)
 
         with self._query_generator() as query:
@@ -494,19 +499,10 @@ class BigQueryRetrievalJob(RetrievalJob):
             # because setting destination for scripts is not valid
             # remove destination attribute if provided
             job_config.destination = None
-            bq_job = self._execute_query(query, job_config, timeout)
+            query = query.replace("__desttable__", str(dest))
+            self._execute_query(query, job_config, timeout)
 
-            if not job_config.dry_run:
-                config = bq_job.to_api_repr()["configuration"]
-                # get temp table created by BQ
-                tmp_dest = config["query"]["destinationTable"]
-                temp_dest_table = f"{tmp_dest['projectId']}.{tmp_dest['datasetId']}.{tmp_dest['tableId']}"
-
-                # persist temp table
-                sql = f"CREATE TABLE {dest} AS SELECT * FROM {temp_dest_table}"
-                self._execute_query(sql, timeout=timeout)
-
-            print(f"Done writing to '{dest}'.")
+            logger.info(f"Done writing to '{dest}'.")
             return str(dest)
 
     def _to_arrow_internal(self, timeout: Optional[int] = None) -> pyarrow.Table:
@@ -534,7 +530,9 @@ class BigQueryRetrievalJob(RetrievalJob):
         assert isinstance(storage, SavedDatasetBigQueryStorage)
 
         self.to_bigquery(
-            bigquery.QueryJobConfig(destination=storage.bigquery_options.table)
+            bigquery.QueryJobConfig(destination=storage.bigquery_options.table),
+            allow_overwrite=allow_overwrite,
+            timeout=7200,
         )
 
     @property
@@ -941,17 +939,19 @@ CREATE TEMP TABLE {{ featureview.name }}__cleaned AS (
  Joins the outputs of multiple time travel joins to a single table.
  The entity_dataframe dataset being our source of truth here.
  */
-
-SELECT {{ final_output_feature_names | join(', ')}}
-FROM entity_dataframe
-{% for featureview in featureviews %}
-LEFT JOIN (
-    SELECT
-        {{featureview.name}}__entity_row_unique_id
-        {% for feature in featureview.features %}
-            ,{% if full_feature_names %}{{ featureview.name }}__{{featureview.field_mapping.get(feature, feature)}}{% else %}{{ featureview.field_mapping.get(feature, feature) }}{% endif %}
-        {% endfor %}
-    FROM {{ featureview.name }}__cleaned
-) USING ({{featureview.name}}__entity_row_unique_id)
-{% endfor %}
+CREATE OR REPLACE TABLE `__desttable__`
+AS (
+    SELECT {{ final_output_feature_names | join(', ')}}
+    FROM entity_dataframe
+    {% for featureview in featureviews %}
+    LEFT JOIN (
+        SELECT
+            {{featureview.name}}__entity_row_unique_id
+            {% for feature in featureview.features %}
+                ,{% if full_feature_names %}{{ featureview.name }}__{{featureview.field_mapping.get(feature, feature)}}{% else %}{{ featureview.field_mapping.get(feature, feature) }}{% endif %}
+            {% endfor %}
+        FROM {{ featureview.name }}__cleaned
+    ) USING ({{featureview.name}}__entity_row_unique_id)
+    {% endfor %}
+);
 """
